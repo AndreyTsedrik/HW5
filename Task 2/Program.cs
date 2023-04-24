825f8e464a12d333066586be290f32d8ff455ee6
AndreyTsedrik <awesome.foxe@yandex.ru>
2023-04-24 12:15:02 +0300

    Task1

---
+++ Task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.WriteLine("Введите размер массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+FillArray(numbers);
+PrintArray(numbers);
+int sum = 0;
+
+for (int j = 0; j < numbers.Length; j+=2)
+sum = sum + numbers[j];
+
+Console.WriteLine($"Сумма чисел с нечетными индексами равна {sum}");
---
+++ Task 3/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArray(numbers);
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int s = 0; s < numbers.Length; s++)
+{
+    if (numbers[s] > max)
+        {
+            max = numbers[s];
+        }
+    if (numbers[s] < min)
+        {
+            min = numbers[s];
+        }
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.WriteLine();
+}
